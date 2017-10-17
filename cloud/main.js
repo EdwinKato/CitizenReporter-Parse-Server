a89b59dae5c8f862d01c3b41304511dd001b4829
Edwin Kato <edwin.kato@andela.com>
2017-10-17 15:03:59 +0300

    Use master key in push

--- cloud/main.js
+++ cloud/main.js
@@ -17,7 +17,11 @@ Parse.Cloud.afterSave("Assignment", function(request) {
             "title": title,
             "description": description
         }
-    }, {
+    },
+        {
+            useMasterKey: true
+        },
+        {
         success: function() {
             console.log("Push notification was sent successfully");
             // Push was successful
@@ -27,3 +31,38 @@ Parse.Cloud.afterSave("Assignment", function(request) {
         }
     });
 });
+
+/*
+Parse.Cloud.afterSave("Comment", function(request) {
+  const query = new Parse.Query("Post");
+  query.get(request.object.get("post").id)
+    .then(function(post) {
+      post.increment("comments");
+      return post.save();
+    })
+    .catch(function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    });
+});
+
+Parse.Cloud.afterSave("MXTMessage", function(request) {
+  var messageText = request.object.get('message');
+  var usersReceived = request.object.get('receiver');
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('user', usersReceived);
+  pushQuery.notEqualTo('user', request.user);
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+        "assignmentId": assignmentId,
+        "title": title,
+        "description": description
+    }
+  }, { useMasterKey: true}).then(() => {
+      // Push was successful
+      console.log('weeee!');
+  }, (e) => {
+      console.log(e);
+  });
+});
+    */
